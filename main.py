5e490d484324295495a373aba0f1cb52c3baac13
Muhammad Qois Huzyan Octava <qoisoctava@gmail.com>
2025-12-20 16:21:44 +0700

    Task 3 added

--- main.py
+++ main.py
@@ -23,6 +23,32 @@ class Order:
         return output
 
 
+# Task 3
+class OrderProcessor:
+    def __init__(self):
+        self.orders = []
+
+    def add_order(self, order):
+        self.orders.append(order)
+
+    def calculate_total_revenue(self):
+        total = 0.0
+        for order in self.orders:
+            total += order["total_amount"]
+        return total
+
+    def calculate_total_tax(self, tax_rate):
+        tax_total = 0.0
+        for order in self.orders:
+            tax_total += order.calculate_tax(tax_rate)
+        return tax_total
+
+    def display_orders(self):
+        for i, order in enumerate(self.orders, start=1):
+            print(f"\n--- Order #{i} ---")
+            order.display_order()
+
+
 def main():
     o1 = Order("ORD-001", "Bambang", "2025-12-18", 250000)
     # print(o1.order_id, o1.customer_name, o1.total_amount)
